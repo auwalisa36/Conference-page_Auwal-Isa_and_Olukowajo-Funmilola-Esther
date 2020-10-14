6e7c73b67585ecb312464648715fc4b7dae38809
Oluwafunmilol <estherolukowajo@yahoo.com>
2020-10-14 15:37:57 +0100

    modified register branch

--- script.js
+++ script.js
@@ -1,47 +1,17 @@
-// function sumbitButton(){
-//   var fullName = '';
-//   var emailAddress = '';
-
-
-//   fullName =document.getElementById( 'fullName').value;
-//   emailAddress =document.getElementById( 'emailAddress').value;
-
-
-//   console.log('Full Name:' +  fullName);
-//   console.log('Email Address:' +  emailAddress);
-
-
-//   if (fullName ==''|| emailAddress =='') {
-//       alert ( 'input this missing field' )
-//   }  
-//   else{
-//       alert('thanks' )
-//   }
-
-// }
-
-
-
-const name = document.querySelector("#fullName");
-const email = document.querySelector("#emailAddress");
-const comment = document.querySelector("#comment");
 const purchaseButton = document.querySelector("#purchasebut");
-// const displayName = document.querySelector(".pname");
-// const displayAge = document.querySelector('.pyears');
-
-purchaseButton.addEventListener("click",function(e){
-    e.preventDefault();
-
+purchaseButton.addEventListener("click",function(){
+    
     let name = document.querySelector("#fullName").value;
     let email = document.querySelector("#emailAddress").value;
+    let noOfPasses = document.querySelector("#numberOfPasses").value;
     let comment = document.querySelector("#comment").value;
-    console.log(name, email, comment);
+    console.log(name, email, noOfPasses, comment);
      
-    if (name == "" || email == "" || comment == ""){
+    if (name == "" || email == "" || noOfPasses == "" || comment == ""){
         alert("all fields required");
         return;
     }
     else {
-      alert("thanks");
-        } 
+      alert("Thanks");
+        } })
 
