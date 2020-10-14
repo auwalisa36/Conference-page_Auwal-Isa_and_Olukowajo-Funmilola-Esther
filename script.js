167024e73d99e71925b475ddb4eeb6c1ffa9a154
Oluwafunmilol <estherolukowajo@yahoo.com>
2020-10-14 17:38:07 +0100

    modified register-branch

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const purchaseButton = document.querySelector("#purchasebut");
+const purchaseButton = document.querySelector(".purchasebut")
 purchaseButton.addEventListener("click",function(){
     
-    let name = document.querySelector("#fullName").value;
-    let email = document.querySelector("#emailAddress").value;
-    let noOfPasses = document.querySelector("#numberOfPasses").value;
-    let comment = document.querySelector("#comment").value;
-    console.log(name, email, noOfPasses, comment);
+    let name = document.getElementById("fullName").value;
+    let email = document.getElementById("email").value;
+    let noOfPasses = document.querySelector(".numberOfPasses").value;
+    let comment = document.querySelector(".comments").value;
+    // console.log(name, email, noOfPasses, comment);
      
     if (name == "" || email == "" || noOfPasses == "" || comment == ""){
         alert("all fields required");
