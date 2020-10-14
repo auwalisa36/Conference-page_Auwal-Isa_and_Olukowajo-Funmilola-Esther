5ce6ed34154e48fdbce89e982916dda96a8ee474
Oluwafunmilol <estherolukowajo@yahoo.com>
2020-10-14 14:15:51 +0100

    modified register-branch

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-function sumbitButton(){
-  var fullName = '';
-  var emailAddress = '';
+// function sumbitButton(){
+//   var fullName = '';
+//   var emailAddress = '';
 
 
-  fullName =document.getElementById( 'fullName').value;
-  emailAddress =document.getElementById( 'emailAddress').value;
+//   fullName =document.getElementById( 'fullName').value;
+//   emailAddress =document.getElementById( 'emailAddress').value;
 
 
-  console.log('Full Name:' +  fullName);
-  console.log('Email Address:' +  emailAddress);
+//   console.log('Full Name:' +  fullName);
+//   console.log('Email Address:' +  emailAddress);
 
 
-  if (fullName ==''|| emailAddress =='') {
-      alert ( 'input this missing field' )
-  }  
-  else{
-      alert('thanks' )
-  }
+//   if (fullName ==''|| emailAddress =='') {
+//       alert ( 'input this missing field' )
+//   }  
+//   else{
+//       alert('thanks' )
+//   }
 
-}
+// }
+
+
+
+const name = document.querySelector("#fullName");
+const email = document.querySelector("#emailAddress");
+const comment = document.querySelector("#comment");
+const purchaseButton = document.querySelector("#purchasebut");
+// const displayName = document.querySelector(".pname");
+// const displayAge = document.querySelector('.pyears');
+
+purchaseButton.addEventListener("click",function(e){
+    e.preventDefault();
+
+    let name = document.querySelector("#fullName").value;
+    let email = document.querySelector("#emailAddress").value;
+    let comment = document.querySelector("#comment").value;
+    console.log(name, email, comment);
+     
+    if (name == "" || email == "" || comment == ""){
+        alert("all fields required");
+        return;
+    }
+    else {
+      alert("thanks");
+        } 
+
