3f70fb04e87d25e9fceba1c8485264c501c12508
Oluwafunmilol <estherolukowajo@yahoo.com>
2020-10-15 09:35:29 +0100

    modified register-branch

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ purchaseButton.addEventListener("click",function(){
     let email = document.getElementById("email").value;
     let noOfPasses = document.querySelector(".numberOfPasses").value;
     let comment = document.querySelector(".comments").value;
-    // console.log(name, email, noOfPasses, comment);
+    console.log(name, email, noOfPasses, comment);
      
     if (name == "" || email == "" || noOfPasses == "" || comment == ""){
         alert("all fields required");
